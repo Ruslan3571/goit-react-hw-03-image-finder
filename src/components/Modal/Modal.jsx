faa91c598989ae09f57aec15de56510f2138b0c1
Ruslan Formanchuk <ruslan12032@gmail.com>
2023-02-02 10:37:23 +0200

    fix

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,6 +1,5 @@
 import { Component } from 'react';
 import { createPortal } from 'react-dom';
-import PropTypes from 'prop-types';
 import s from './Modal.module.css';
 
 const modalRoot = document.getElementById('modal');
